58551e1ae6ac01d4fc2d800f647d12f39d216666
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-26 03:26:44 +0530

    EventHandling

--- DelegatesAndEvents/Delegates.cs
+++ DelegatesAndEvents/Delegates.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    internal class Delegates
+    public class Delegates
     {
+        public static void Addition(int a, int b)
+        {
+            Console.WriteLine("Addition is : " + (a + b));
+        }
+        public static void Substraction(int a, int b)
+        {
+            Console.WriteLine("Substraction is : " + (a - b));
+        }
+        public static void Multiplication(int a, int b)
+        {
+            Console.WriteLine("Multiplication is : " + (a * b));
+        }
+    }
+    public class ArrayDelegates
+    {
+        delegate void Del(int x, int y);
+
+        public static void ImplementDelegates()
+        {
+            Del obj = new Del(Delegates.Addition);
+            obj += Delegates.Substraction;
+            obj += Delegates.Multiplication;
+            obj(10, 5);
+            Console.WriteLine();
+            obj(20, 10);
+            Console.WriteLine();
+            obj(40, 20);
+        }
     }
 }
--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace DelegatesAndEvents
 {
-    internal class Program
+    public class Program
     {
+        public static event DelEventHandler Del;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("******************** Event Handling *****************************");
+            EventHandling.ImplementEvent();
+
+            Del += new DelEventHandler(EventProblem.India);
+            Del += new DelEventHandler(EventProblem.USA);
+            Del += new DelEventHandler(EventProblem.England);
+            Del.Invoke();
+
+            ArrayDelegates.ImplementDelegates();
         }
     }
 }
