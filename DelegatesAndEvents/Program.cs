73d0ae08325cb18edfd60c992c7ccbfaf179d5c0
Rushikesh041 <jadhavrushi041@gmail.com>
2022-11-26 03:38:13 +0530

    SingleCast

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -2,20 +2,8 @@
 {
     public class Program
     {
-        public static event DelEventHandler Del;
         static void Main(string[] args)
         {
-<<<<<<< HEAD
-=======
-            Console.WriteLine("******************** Event Handling *****************************");
-            EventHandling.ImplementEvent();
-
->>>>>>> EventHandling
-            Del += new DelEventHandler(EventProblem.India);
-            Del += new DelEventHandler(EventProblem.USA);
-            Del += new DelEventHandler(EventProblem.England);
-            Del.Invoke();
-
             ArrayDelegates.ImplementDelegates();
         }
     }
